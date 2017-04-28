e1eab11d3fcbcd523c6e2340bc2be85ce5b3235c
AuliaYF <auliayf@gmail.com>
2017-04-28 23:59:44 +0700

    Separated functions

--- easyraikit.py
+++ easyraikit.py
@@ -1,60 +1,37 @@
-import re
-import math
 import json
 import requests
-import configparser
 from time import sleep
-
-config = configparser.ConfigParser()
-config.read('config.cfg')
-server = config.get('main', 'server')
-wallet = config.get('main', 'wallet')
-wallet_password = config.get('main', 'wallet_password')
-
-def raiblocks_account_validate(account):
-	if isinstance(account, str):
-		if ('xrb_1' in account) or ('xrb_3' in account) and (len(account) == 64):
-			account = account[4:]
-			char_validation = re.search('^[13456789abcdefghijkmnopqrstuwxyz]+$', account)
-			if char_validation is not None:
-				return True
-			else:
-				return False
-		else:
-			return False
-	else:
-		return False
-
-def raiblocks_mrai_from_raw(raw):
-	return int(math.floor(raw / (10 ** 30)))
-
-def raiblocks_mrai_to_raw(mrai):
-	return int(math.floor(mrai * (10 ** 30)))
+from easyraikit_config import *
 
 class Rai:
 	def __getattr__(self, name, *args):
 		def function (*args):
-			request = {}
-			request['action'] = name
-			if args:
-				for key, value in args[0].items():
-					request[key] = value
-			try:
-				r = requests.post(server, data = json.dumps(request)).json()
+			global server
 
-				if 'error' not in r:
-					return(r)
-				else:
-					print(r['error'])
-					return None
-			except:
-				sleep(0.5)
-				r = requests.post('', data = json.dumps(request)).json()
+			if server:
+				request = {}
+				request['action'] = name
+				if args:
+					for key, value in args[0].items():
+						request[key] = value
+				try:
+					r = requests.post(server, data = json.dumps(request)).json()
 
-				if 'error' not in r:
-					return(r)
-				else:
-					print(r['error'])
-					return None
+					if 'error' not in r:
+						return(r)
+					else:
+						print(r['error'])
+						return None
+				except:
+					sleep(0.5)
+					r = requests.post('', data = json.dumps(request)).json()
+
+					if 'error' not in r:
+						return(r)
+					else:
+						print(r['error'])
+						return None
+			else:
+				print('Wrong server configuration.')
 
 		return function
---
+++ easyraikit_ext.py
@@ -0,0 +1,37 @@
+import re
+import math
+from easyraikit_config import *
+from easyraikit import Rai
+
+rai = Rai()
+
+def raiblocks_account_validate(account):
+	if isinstance(account, str):
+		if ('xrb_1' in account) or ('xrb_3' in account) and (len(account) == 64):
+			account = account[4:]
+			char_validation = re.search('^[13456789abcdefghijkmnopqrstuwxyz]+$', account)
+			if char_validation is not None:
+				return True
+			else:
+				return False
+		else:
+			return False
+	else:
+		return False
+
+def raiblocks_mrai_from_raw(raw):
+	return int(math.floor(raw / (10 ** 30)))
+
+def raiblocks_mrai_to_raw(mrai):
+	return int(math.floor(mrai * (10 ** 30)))
+
+def raiblocks_unlock():
+	global rai
+	global wallet
+	global wallet_password
+
+	valid = rai.password_enter({'wallet': wallet, 'password': wallet_password})
+	if valid is not None:
+		return valid['valid']
+	else:
+		return "Wallet unlock failed."
